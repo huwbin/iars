8d78e56844056cf24565fcb8c97c4b92205b562c
huw <95523894+huwbin@users.noreply.github.com>
2023-02-13 14:25:43 +0000

    fff

---
+++ lib/pages/pictures.dart
@@ -0,0 +1,32 @@
+//pictures page
+import 'package:flutter/material.dart';
+
+
+//pictures page json file connection to stu twittwer or facebook
+
+
+class PicturesPage extends StatelessWidget [
+  const PicturesPage({=PicturesPage()
+  @override
+class PicturesPageState {
+  return materialApp(
+      appBar: AppBar(title: (const Text('Pictures')),
+      flexibleSpace: Container(alignment: Alignment.centerRight,
+      color: Colors.blue,      
+      child: const Icon(Icons.search),
+      ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: const <Widget>[
+            Text('huw!')
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+\\
---
+++ lib/pages/widgets/material.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class pic extends StatefulWidget {
+  const pic({super.key});
+
+  @override
+  State<pic> createState() => _picState();
+}
+
+class _picState extends State<pic> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+//pictures page json file connection to stu twittwer or facebook
